6ea0c263b91fc03add082b6a158cbe338847a1bc
daigo <hal.60445.kobayashi@gmail.com>
2019-12-25 02:28:52 +0900

    googleの認証追加

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,37 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    message:"",
-    count:0
+    message: "",
+    count: 0,
+    user_prof_name: "",
+    user_prof_image: ""
   },
   mutations: {
-    send_message(send_message){
-      this.state.message=send_message
+    send_message(send_message) {
+      this.state.message = send_message;
     },
-    increment(state,message){
-      
-      console.log(message);
-      this.state.message=message
+    increment(state, message) {
+      this.state.message = message;
+    },
+    sign_in(state, user) {
+      state.user_prof_name = user.user.displayName;
+      state.user_prof_image = user.user.photoURL;
     }
   },
   actions: {
-    send_message(context){
+    send_message(context) {
       console.log(context);
     },
-    increment(context,payload){
-      context.commit('increment',payload.message)
+    increment(context, payload) {
+      context.commit("increment", payload.message);
+    },
+    sign_in(context, user) {
+      context.commit("sign_in", user);
     }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
