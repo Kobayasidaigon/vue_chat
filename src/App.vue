de79cd68e18f1f9d9b38ef44ce3f81f4b44e87e9
daigo <hal.60445.kobayashi@gmail.com>
2019-12-26 03:29:20 +0900

    メッセージ送信の変更

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <button @click="sign_in">ログイン</button>
-    {{this.$store.state.user_prof_name}}
-    <img
-      :src="this.$store.state.user_prof_image"
-      width="40"
-      height="40"
-    />
+    <div v-if="this.$store.state.user_prof_name!='ユーザーA'">
+      {{ this.$store.state.user_prof_name }}
+      <button @click="sign_out">ログアウト</button>
+      <img :src="this.$store.state.user_prof_image" width="40" height="40" />
+    </div>
+    <div v-else>
+      <p>{{ this.$store.state.user_prof_name }}</p>
+      <button @click="sign_in">ログイン</button>
+    </div>
     <p>メッセージをいれてね</p>
     <input type="text" v-model="message" />
     <button @click="send">send</button>
     <p>{{ this.$store.state.message }}</p>
-    <li v-for="(value,index, key) in messageList" :key="key">{{value}}</li>
+    <div v-for="(message, index, key) in messageList" :key="key" style="display: block;">
+      <img :src="message.user_image" width="40" height="40" />
+      <p>{{ message.message }}</p>
+      <p>{{ message.user_name }}</p>
+      <hr />
+    </div>
   </div>
 </template>
 
@@ -23,7 +30,7 @@ export default {
   data() {
     return {
       message: "hello world",
-      user_name: "",
+      user_name: "ユーザー",
       user_image: "",
       messageList: []
     };
@@ -33,23 +40,23 @@ export default {
       const provider = new firebase.auth.GoogleAuthProvider();
       firebase.auth().signInWithPopup(provider);
     },
+    sign_out: function() {
+      firebase.auth().signOut();
+      this.$store.dispatch("sign_out");
+    },
     send: function() {
       db.collection("message").add({
         message: this.message,
         user_name: this.$store.state.user_prof_name,
         user_image: this.$store.state.user_prof_image
       });
-      db.collection("message")
-        .get()
-        .then(snapshot => {
-          let messageList = [];
-          snapshot.forEach(doc => {
-            messageList.push(doc.data().message);
-            messageList.push(doc.data().user_name);
-            messageList.push(doc.data().user_image);
-          });
-          this.messageList = messageList;
-        });
+
+      let data = {
+        message: this.message,
+        user_name: this.$store.state.user_prof_name,
+        user_image: this.$store.state.user_prof_image
+      };
+      this.messageList.push(data);
     }
   },
   created: function() {
@@ -73,6 +80,11 @@ export default {
       if (user) {
         //ログイン状態
         this.$store.dispatch("sign_in", { user: user });
+        console.log(
+          user != ""
+            ? (this.user_name = this.$store.state.user_prof_name)
+            : (this.user_name = "ユーザーA")
+        );
       }
     });
   }
